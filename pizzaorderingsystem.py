a3405935fa7f084ad3e8fb93e70621f12270cd3e
yiitdotdev <125571975+yiitdotdev@users.noreply.github.com>
2023-03-05 18:34:32 +0300

    Update pizzaorderingsystem.py

--- pizzaorderingsystem.py
+++ pizzaorderingsystem.py
@@ -122,14 +122,73 @@ class PizzaOrder:
         for pizza_with_toppings in self.pizzas:
             pizza, toppings = pizza_with_toppings
             print(f"{pizza.get_name()} ({pizza.get_codeid()})")
-            print(f"\tDescription: {pizza.get_description()}")
-            print(f"\tCost: ${pizza.get_cost()}")
+            print(f"\tAçıklama: {pizza.get_description()}")
+            print(f"\tTutar: {pizza.get_cost()} ₺")
 
             for topping in toppings:
                 print(f"\t{ToppingDecorator.get_name(topping)} ({ToppingDecorator.get_codeid(topping)})")
-                print(f"\t\tDescription: {ToppingDecorator.get_description(topping)}")
-                print(f"\t\tCost: ${ToppingDecorator.get_cost(topping)}")
+                print(f"\t\tAçıklama: {ToppingDecorator.get_description(topping)}")
+                print(f"\t\tTutar: {ToppingDecorator.get_cost(topping)} ₺")
 
             total_cost += pizza.get_cost() + sum([topping.get_cost() for topping in toppings])
 
-        print(f"\nTotal cost: ${total_cost}")
+        print(f"\nTOTAL: {total_cost} ₺")
+
+# pizzaları listelemek için boş liste oluşturuyoruz.
+pizzas = []
+
+# Kullanıcıdan değer alma işlemi
+while True:
+    choice = input("Pizza Kodunu giriniz!(a1-a4) (Sepete gitmek için: 'done'): ")
+    if choice.lower() == 'done':
+        break
+    elif choice.lower() == 'a1':
+        new_pizza = klasik()
+    elif choice.lower() == 'a2':
+        new_pizza = margarita()
+    elif choice.lower() == 'a3':
+        new_pizza = turkishpizza()
+    elif choice.lower() == 'a4':
+        new_pizza = sade()
+    else:
+        print("Geçersiz kod. Tekrar deneyin.")
+        continue
+
+    # Pizzaya sos ekleme işlemi - input
+    while True:
+        topping_choice = input(f" Seçtin:  {new_pizza.get_name()}. \n Sos eklemek ister misin? (y/n): ")
+        if topping_choice.lower() == 'y':
+            topping_code = input("Sos Kodu giriniz (x1 - x6): ")
+            if topping_code.lower() == 'x1':
+                new_pizza = Zeytin(new_pizza)
+            elif topping_code.lower() == 'x2':
+                new_pizza = Mantar(new_pizza)
+            elif topping_code.lower() == 'x3':
+                new_pizza = Kecipeyniri(new_pizza)
+            elif topping_code.lower() == 'x4':
+                new_pizza = Et(new_pizza)
+            elif topping_code.lower() == 'x5':
+                new_pizza = Sogan(new_pizza)
+            elif topping_code.lower() == 'x6':
+                new_pizza = Misir(new_pizza)
+            else:
+                print("Geçersiz kod. Tekrar deneyin")
+                continue
+        elif topping_choice.lower() == 'n':
+            break
+        else:
+            print("Geçersiz kod. Evet için 'y', Hayır için 'n'.")
+            continue
+
+    # oluşturulan pizzayı listeye ekleme
+    pizzas.append(new_pizza)
+
+    # onay mesajı
+    print(f"{new_pizza.get_name()} Sepete eklendi.")
+
+order = PizzaOrder()
+for pizza in pizzas:
+    order.add_pizza(pizza, [])
+
+# fiş yazdırma
+order.print_receipt()
